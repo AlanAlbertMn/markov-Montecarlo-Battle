dd531964af48c674a898794ecccd6e7444a2b69e
AlanAlbertMn <A01328928@itesm.mx>
2021-11-26 15:20:09 -0600

    Clan annihilation implemented

--- battle.py
+++ battle.py
@@ -4,10 +4,24 @@ import numpy as np
 a = 0
 b = 1
 N = 1
+filename = 'battleResults.txt'
 
 # function for montecarlo
 def f(x):
     return x**2
+
+
+def printMatrix(matrix, size):
+    for i in range(size):
+        file.write("  {}\t" .format(i))
+    file.write("\n")
+    row = 0
+    for i in matrix:
+        file.write("{} " .format(row))
+        for j in i:
+            file.write("{} " .format(str(float(j))))
+        file.write("\n")
+        row+=1
 
 def montecarlo(probabilities):
     for i in range(N):
@@ -22,23 +36,27 @@ def montecarlo(probabilities):
 
         ans = (b-a)/float(N)*integral
     
-    print("Ans ", ans)
+    # print("Ans ", ans)
     i = 0
     tmp = i
     for x in probabilities:
         # print("Interval [{}, {}]" .format(x[i], x[i+1]))
         if ans >= x[i] and ans < x[i+1]:
-            print("Ans {} in {} interval" .format(ans, tmp))
+            # print("Ans {} in {} interval" .format(ans, tmp))
+            # file.write("Ans {} in {} interval\n" .format(ans, tmp))
             return tmp
         else:
             # print("Ans {} not in {} interval" .format(ans, tmp))
             tmp+=1
 
 def numSoldiers(dictClans):
-    print("=================================")
+    # print("=================================")
+    # file.write("=================================\n")
     for clan, info in dictClans.items():
-        print("Clan {} has {} soldiers left." .format(clan, info[0]))
-    print("=================================")
+        # print("Clan {} has {} soldiers left." .format(clan, info[0]))
+        file.write("Clan {} has {} soldiers left.\n" .format(clan, info[0]))
+    # print("=================================")
+    file.write("=================================\n")
     
 def getIntervals(probListAttacker, attacker):
     inter = list()
@@ -54,16 +72,28 @@ def getIntervals(probListAttacker, attacker):
     # print(inter)
     return inter
 
+def countSoldiers(clans):
+    for clan, info in clans.items():
+        print(info[0])
+        if info[0] == 0:
+            print("Clan {} was annihilated" .format(clan))
+            clans.pop(clan)
+            return 0
+
+
 def attackSoldier(dictClans, attacker):
     probs = dictClans.get(attacker)[2]
-    print("Probabilities of attacking for clan ", attacker)
-    print(probs)
+    # print("Probabilities of attacking for clan ", attacker)
+    # print(probs)
+    # file.write("Probabilities of attacking for clan {}\n" .format(attacker))
     attacked = montecarlo(probs)
     if attacked == attacker: # this condition is set so if montecarlo generates the same clan as objective for the attackiong clan, it generates a new objective
         while attacked == attacker:
             attacked = montecarlo(probs)
-            print("Attacked clan number: ", attacked)
-    print("Clan {} attacked clan {}!" .format(attacker, attacked))
+            # print("Attacked clan number: ", attacked)
+            file.write("Attacked clan number: {}\n" .format(attacked))
+    file.write("Clan {} attacked clan {}!\n" .format(attacker, attacked))
+    # print("Clan {} attacked clan {}!" .format(attacker, attacked))
     dictClans.get(attacked)[0]-=1
 
 
@@ -75,15 +105,29 @@ battleMatrix = [[ 0 , 0.4, 0.6],
 # in 'clans' is saved the required information of the clans in the following structure: {'number_of_clan': ['amount_of_soldiers', 'probabilities_to_attack', 'intervals_for_montecarlo']}
 clans = dict({0: [10, battleMatrix[0], getIntervals(battleMatrix[0], 0)], 1: [20, battleMatrix[1], getIntervals(battleMatrix[1], 1)], 2: [30, battleMatrix[2], getIntervals(battleMatrix[2], 2)]})
 
-# print(clans)
-numSoldiers(clans)
 
-# generate random number of the clan that is going to attack
-rClan = random.randint(0, len(clans)-1)
-print("Attacking clan: " , rClan)
+##################################################################################################################
+# Start
+##################################################################################################################
 
-# then we call the function giving permission to the selected clan to attack other clans
-attackSoldier(clans, rClan)
+# initial state of the clans
 
-# Now we print the current status of the battle
-numSoldiers(clans)
+with open(filename,"w+",encoding="utf-8") as file:
+    printMatrix(battleMatrix, 3)
+    numSoldiers(clans)
+    for i in range(25):
+        # generate random number of the clan that is going to attack
+        rClan = random.randint(0, len(clans)-1)
+        # print("Attacking clan: " , rClan)
+
+        file.write("Attacking clan: {}\n" .format(rClan))
+        # then we call the function giving permission to the selected clan to attack other clans
+        attackSoldier(clans, rClan)
+        
+        # We need to know if any clan ran out of soldiers
+        if countSoldiers(clans) == 0:
+            print("Somebody ran out of soldiers")
+            numSoldiers(clans)
+            break
+        # Now we print the current status of the battle
+        numSoldiers(clans)
